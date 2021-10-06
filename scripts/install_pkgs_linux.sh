a5265d33c83d8443683348875a312027d3e7600b
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2021-10-06 23:13:35 +0900

    fix install_pkgs script

--- scripts/install_pkgs_linux.sh
+++ scripts/install_pkgs_linux.sh
@@ -4,7 +4,8 @@ apt-get install -y libobs-dev
 apt-get install python3-pip
 pip3 install cmake
 
-DEPS_DIR=$(dirname $0)/../deps
+DEPS_DIR=$(cd $(dirname $0)/..;pwd)/deps
+[ -d ${DEPS_DIR} ] || mkdir ${DEPS_DIR}
 
 curl -L -o ${DEPS_DIR}/llvm.sh https://apt.llvm.org/llvm.sh
 chmod +x ${DEPS_DIR}/llvm.sh
