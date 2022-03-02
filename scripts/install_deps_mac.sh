49bc2bd73a03205af5e2799c8c565a2da90592a8
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2022-03-02 22:14:43 +0900

    support m1...?

--- scripts/install_deps_mac.sh
+++ scripts/install_deps_mac.sh
@@ -9,9 +9,17 @@ HALIDE_URL=https://github.com/halide/Halide/releases/download/v13.0.0/Halide-13.
 HALIDE_TGZ=${DEPS_DIR}/Halide-${HALIDE_VERSION}.tar.gz
 HALIDE_DIR=${DEPS_DIR}/Halide
 
+ONNXRUNTIME_VERSION=1.10.0
+ONNXRUNTIME_URL=https://github.com/microsoft/onnxruntime/releases/download/v${ONNXRUNTIME_VERSION}/onnxruntime-osx-universal2-${ONNXRUNTIME_VERSION}.tgz
+ONNXRUNTIME_TGZ=${DEPS_DIR}/onnxruntime-osx-universal2-${ONNXRUNTIME_VERSION}.tgz
+ONNXRUNTIME_DIR=${DEPS_DIR}/onnxruntime
+
 [ -d ${DEPS_DIR} ] || mkdir ${DEPS_DIR}
 [ -e ${HALIDE_TGZ} ] || curl -o ${HALIDE_TGZ} -L ${HALIDE_URL}
 [ -d ${HALIDE_DIR} ] || mkdir ${HALIDE_DIR} && tar zxf ${HALIDE_TGZ} -C ${HALIDE_DIR} --strip-components 1
+
+[ -e ${ONNXRUNTIME_TGZ} ] || curl -o ${ONNXRUNTIME_TGZ} -L ${ONNXRUNTIME_URL}
+[ -d ${ONNXRUNTIME_DIR} ] || mkdir ${ONNXRUNTIME_DIR} && tar zxf ${ONNXRUNTIME_TGZ} -C ${ONNXRUNTIME_DIR} --strip-components 1
 
 # for update obs version.
 brew update
