5152aa463ebdf9039b8a5de7f6c5157cf8d84698
KOUNOIKE Yuusuke <yusuke.konoike@conoc.jp>
2022-04-02 03:58:03 +0900

    build re2/nsync manually

--- scripts/build_onnxruntime_mac.sh
+++ scripts/build_onnxruntime_mac.sh
@@ -9,7 +9,7 @@ ORT_VERSION=1.11.0
 mkdir -p deps/onnxruntime
 
 # git -C deps clone --single-branch --depth 1 -b v${ORT_VERSION} https://github.com/microsoft/onnxruntime.git
-git -C deps clone --single-branch --depth 1 -b master https://github.com/microsoft/onnxruntime.git
+git -C deps clone --recursive --single-branch --depth 1 -b master https://github.com/microsoft/onnxruntime.git
 pushd deps/onnxruntime
   ./build.sh --config RelWithDebInfo --parallel \
     --use_coreml \
@@ -23,6 +23,22 @@ pushd deps/onnxruntime
     cmake --build . --target install
     find . -name '*.a' -exec cp {} $(cd ../../../..; pwd)/onnxruntime/lib/ \;
     # cmake --install . --prefix=$(cd ../../../..; pwd)/onnxruntime --config RelWithDebInfo
+  popd
+  pushd cmake/external/re2
+    mkdir b
+    pushd b
+      cmake -DCMAKE_APPLE_SILICON_PROCESSOR=x86_64 -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 ..
+      cmake --build . --target re2
+      cp libre2.a ../../../../lib/
+    popd
+  popd
+  pushd cmake/external/nsync
+    mkdir b
+    pushd b
+      cmake -DCMAKE_APPLE_SILICON_PROCESSOR=x86_64 -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 ..
+      cmake --build . --target nsync_cpp
+      cp libnsync_cpp.a ../../../../lib/
+    popd
   popd
   cp -f include/onnxruntime/core/session/*.h include/onnxruntime/core/providers/coreml/*.h include/
 popd
