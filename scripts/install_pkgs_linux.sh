44cc85612eaebacb6b9149a0ea0f9a9d5f66e60a
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2022-04-06 20:09:28 +0900

    use cpack for package deb & build problem fix

--- scripts/install_pkgs_linux.sh
+++ scripts/install_pkgs_linux.sh
@@ -8,8 +8,3 @@ pip3 install cmake
 
 DEPS_DIR=$(cd $(dirname $0)/..;pwd)/deps
 [ -d ${DEPS_DIR} ] || mkdir ${DEPS_DIR}
-
-curl -L -o ${DEPS_DIR}/llvm.sh https://apt.llvm.org/llvm.sh
-chmod +x ${DEPS_DIR}/llvm.sh
-${DEPS_DIR}/llvm.sh 12
-apt-get install libc++-12-dev libc++1-12 libc++abi1-12
