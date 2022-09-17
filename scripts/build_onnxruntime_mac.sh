973a515b9322cd74d329372883611d52b46aa42f
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2022-09-17 13:57:55 +0900

    fix command line

--- scripts/build_onnxruntime_mac.sh
+++ scripts/build_onnxruntime_mac.sh
@@ -13,7 +13,7 @@ ORT_TGZ=./deps/onnxruntime-${ORT_VERSION}.tar.gz
 
 curl -L -o $ORT_TGZ $ORT_URL 
 mkdir -p deps/onnxruntime
-tar zxf -C deps/onnxruntime --strip-components=1 $ORT_TGZ
+tar -C deps/onnxruntime --strip-components=1 zxf $ORT_TGZ
 
 pushd deps/onnxruntime
   ./build.sh --config RelWithDebInfo --parallel \
